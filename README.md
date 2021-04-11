4829c0e1540b77dcc3a881b1f8e8d4976244b6e7
Levio <js_levi@163.com>
2021-04-11 21:03:43 +0800

    sql

--- README.md
+++ README.md
@@ -5,4 +5,4 @@
 - ts
 - swagger
 - log
--
+- db
--- bin/www.ts
+++ bin/www.ts
@@ -7,15 +7,47 @@ dotenv.config();
 import http from "http";
 import app from "../src/app";
 import logger from "../src/logs/logger";
+import {createConnection} from 'typeorm';
+import 'reflect-metadata';
 
 const port = normalizePort(process.env.PORT || "3000");
 
-const server = http.createServer(app.callback());
+createConnection().then(() => {
 
-server.listen(port);
-logger().debug(`Server is listening http://localhost:${port}`);
-server.on("error", onError);
-server.on("listening", onListening);
+  const server = http.createServer(app.callback());
+
+  server.listen(port);
+  server.on("error", onError);
+  server.on("listening", onListening);
+  
+  function onError(error: any) {
+    if (error.syscall !== "listen") {
+      throw error;
+    }
+    const bind = typeof port === "string" ? "Pipe " + port : "Port " + port;
+    switch (error.code) {
+      case "EACCES":
+        logger().error(bind + " requires elevated privileges");
+        process.exit(1);
+      case "EADDRINUSE":
+        logger().error(bind + " is already in use");
+        process.exit(1);
+      default:
+        throw error;
+    }
+  }
+  
+  function onListening() {
+    const addr = server.address();
+    const bind = typeof addr === "string" ? "pipe " + addr : "port " + addr?.port;
+    logger().debug("Listening on " + bind);
+    logger().debug(`Server is listening http://localhost:${port}`);
+  }
+
+}).catch(error => {
+  logger().error("TypeORM connection error:", error);
+})
+
 
 function normalizePort(val: any) {
   const port = parseInt(val, 10);
@@ -28,25 +60,3 @@ function normalizePort(val: any) {
   return false;
 }
 
-function onError(error: any) {
-  if (error.syscall !== "listen") {
-    throw error;
-  }
-  const bind = typeof port === "string" ? "Pipe " + port : "Port " + port;
-  switch (error.code) {
-    case "EACCES":
-      logger().error(bind + " requires elevated privileges");
-      process.exit(1);
-    case "EADDRINUSE":
-      logger().error(bind + " is already in use");
-      process.exit(1);
-    default:
-      throw error;
-  }
-}
-
-function onListening() {
-  const addr = server.address();
-  const bind = typeof addr === "string" ? "pipe " + addr : "port " + addr?.port;
-  logger().debug("Listening on " + bind);
-}
